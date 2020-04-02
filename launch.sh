bf67e71f8bc98ce4786b5d2e1cfc49e13628411c
marschla <59028617+marschla@users.noreply.github.com>
2020-04-02 14:15:44 +0200

    Initial commit

---
+++ launch.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -e
+
+# YOUR CODE BELOW THIS LINE
+# ----------------------------------------------------------------------------
+roslaunch my_package node.launch
+#rosrun my_package sub.py
