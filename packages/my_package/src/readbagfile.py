5465bae8359e5377b14d1f1dca71aec27189e247
marschla <marschla@student.ethz.ch>
2020-06-22 13:40:26 +0200

    read filename from cmd line

--- packages/my_package/src/readbagfile.py
+++ packages/my_package/src/readbagfile.py
@@ -20,22 +20,44 @@ class MyNode(DTROS):
     def run(self):
 
         #name of bagfile/txtfile
-        name = "PID_debug"
+        #name = "LFdemo2"
+
+        #name from commandline
+        name = os.environ['FILENAME']
+
+        rospy.loginfo(name)
 
         #choose name of txt file (choose folder according to volume mounting)
-        file = open("/home/"+name+"_lp.txt","w")
+        file1 = open("/home/"+name+"_omega.txt","w")
+        file2 = open("/home/"+name+"_v.txt","w")
 
         #name bag to analyze (attention volume mounting)
         bag = rosbag.Bag('/home/'+name+'.bag')
         #choose topic
-        for topic, msg, t in bag.read_messages("/marschla2/lane_filter_node/lane_pose"):
+        '''
+        for topic, msg, t in bag.read_messages("/autobot17/lane_filter_node/lane_pose"):
             #rospy.loginfo(msg)
             file.write(str(msg))
+        '''
+        
+        arr_omega = []
+        arr_v = []
 
+        for topic, msg, t in bag.read_messages("/autobot17/wheels_driver_node/wheels_cmd_decalibrated"):
+            #rospy.loginfo(msg.format)
+            omega = (msg.vel_left-msg.vel_right)/2.0
+            v = (msg.vel_left+msg.vel_right)/2.0
+            #rospy.loginfo("omega: %s" % omega)
+            arr_omega.append(omega)
+            arr_v.append(v)
+            
+        np.savetxt(file1,arr_omega)
+        np.savetxt(file2,arr_v)
         bag.close()
-        file.close()
+        file1.close()
+        file2.close()
 
-
+        '''
         #choose name of txt file (choose folder according to volume mounting)
         file = open("/home/"+name+"_wd.txt","w")
 
@@ -45,9 +67,10 @@ class MyNode(DTROS):
         for topic, msg, t in bag.read_messages("/marschla2/wheels_driver_node/wheels_cmd"):
             #rospy.loginfo(msg)
             file.write(str(msg))
-
+        
         bag.close()
         file.close()
+        '''
 
 
 
